c798b4bd7fd64d387cac7187133ad9ce5230a67b
Nurken <nurkenqaldybaev2001@gmail.com>
2025-05-23 15:33:16 +0500

    port

--- client/src/config/axios.config.js
+++ client/src/config/axios.config.js
@@ -5,7 +5,7 @@ console.log("SERVER_BASE_URL:", import.meta.env.VITE_SERVER_BASE_URL);
 const customAxios = axios.create({
     baseURL:process.env.VITE_SERVER_BASE_URL,
     withCredentials: true,
-    timeout:10000
+    timeout:20000
 })
 
 export default customAxios
